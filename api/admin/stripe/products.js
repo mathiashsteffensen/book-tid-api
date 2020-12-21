51ed4bba13b40e7f752948ba62d39b9bb5d570d5
Mathias <mathias@mathiashsteffensen@protonmail.com>
2020-12-21 23:36:56 +0100

    added bug fix

--- api/admin/stripe/products.js
+++ api/admin/stripe/products.js
@@ -1,11 +1,15 @@
-const stripe = require('../../../stripe')
+const Stripe = require('stripe')
+
 const express = require('express')
 const bodyParser = require('body-parser')
 
 const productsRouter = express.Router()
 
-productsRouter.get('/products-and-prices', async (req, res, next) =>
+productsRouter.get('/products-and-prices/:env', async (req, res, next) =>
 {
+    let key = req.params.env === 'production' ? process.env.STRIPE_SECRET_KEY : process.env.TEST_STRIPE_SECRET_KEY
+    const stripe = Stripe(key)
+
     const prices = (await stripe.prices.list({active: true})).data
     const products = await Promise.all(prices.map(async price => 
         {
