b0df6b14eef2ff49628748bff69e1e8dda3a4e2b
Mathias <mathias@mathiashsteffensen@protonmail.com>
2020-12-22 01:07:34 +0100

    Made sure pricing is included properly in API calls

--- api/admin/stripe/products.js
+++ api/admin/stripe/products.js
@@ -10,7 +10,7 @@ productsRouter.get('/products-and-prices/:env', async (req, res, next) =>
     let key = req.params.env === 'production' ? process.env.STRIPE_SECRET_KEY : process.env.TEST_STRIPE_SECRET_KEY
     const stripe = Stripe(key)
 
-    const prices = (await stripe.prices.list({active: true})).data
+    const prices = (await stripe.prices.list({active: true}, {expand: ['tiers']})).data
     const products = await Promise.all(prices.map(async price => 
         {
             const product = await stripe.products.retrieve(price.product)
