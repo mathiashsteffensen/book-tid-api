8adb934e8808d854bb02733496b188895db6bbc9
Mathias <mathias@mathiashsteffensen@protonmail.com>
2021-03-02 16:43:14 +0100

    Added sysadmin model

--- api/api.js
+++ api/api.js
@@ -5,11 +5,13 @@ const {errorHandler} = require('../middleware')
 const apiRouter = express.Router()
 
 // Importing APIs
-const adminRouter = require('./admin/admin')
-const clientRouter = require('./client/index')
+const adminRouter = require('./admin')
+const clientRouter = require('./client')
+const sysadminRouter = require('./sysadmin')
 
 apiRouter.use('/admin', adminRouter)
 apiRouter.use('/client', clientRouter)
+apiRouter.use('/sysadmin', sysadminRouter)
 
 apiRouter.use('*', errorHandler)
 
--- db/models.js
+++ db/models.js
@@ -3,6 +3,18 @@ const mongoose = require('mongoose')
 
 /*** Create Schemas ***/
 
+// SysadminSchema
+const SysadminSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
 // BookingSettingsSchema 
 const BookingSettingsSchema = new mongoose.Schema({
     domainPrefix: {
@@ -592,6 +604,7 @@ const TextReminderAppSchema = new mongoose.Schema({
 })
 
 // Configuring schemas to models and exporting them
+const Sysadmin = mongoose.model('Sysadmin', SysadminSchema)
 const AdminClient = mongoose.model('AdminClient', AdminClientSchema)
 const AdminCalendar = mongoose.model('AdminCalendar', AdminCalendarSchema)
 const Customer = mongoose.model('Customer', CustomerSchema)
@@ -601,6 +614,7 @@ const Service = mongoose.model('Service', ServiceSchema)
 const TextReminderApp = mongoose.model('TextReminderApp', TextReminderAppSchema)
 
 module.exports = {
+    Sysadmin,
     AdminClient,
     AdminCalendar,
     Customer,
